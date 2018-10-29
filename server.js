04ba8528d9fb01897c6fd0c5c0cf66d87370eb6e
Christopher Dennis <cdennis.aus@gmail.com>
2018-10-28 23:08:48 -0500

    Initial server setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Import routes
+const users = require('./routes/api/users');
+
+const app = express();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Test');
+});
+
+// Use routes
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
