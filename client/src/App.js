d05b0af2e442fe4bfb7b4fa457e783c49487f1d2
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-01 22:10:01 -0500

    Created initial client side React component

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './main.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id='container'>
+        <h1>MERN Social</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
