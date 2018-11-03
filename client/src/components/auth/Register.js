d017022ab78711c74857671cb0e3c7400c14963c
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-02 23:11:27 -0500

    Created initial register component

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return (
+      <div id='register'>
+        <h2>Register Compoent</h2>
+      </div>
+    );
+  }
+}
+
+export default Register;
