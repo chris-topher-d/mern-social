f10f1e90a3856fdbec0849b4982b5e468abfcd86
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-02 23:12:16 -0500

    Added React Router and initial routes for login/register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './main.css';
 
 class App extends Component {
   render() {
     return (
-      <div id='container'>
-        <h1>MERN Social</h1>
-      </div>
+      <Router>
+        <div id='app'>
+          <h1>MERN Social</h1>
+          <Route exact path='/' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </div>
+      </Router>
     );
   }
 }
