d36efd767fc517a0f62233a46ae6baa87e480f4d
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-02 23:11:09 -0500

    Created initial login component

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div id='login-container'>
+        <h2>Login Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Login;
