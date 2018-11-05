8733219d3e5c7522cde810a63f635dc2ffb5e807
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-04 19:59:04 -0600

    Mocked up initial login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { loginUser } from '../../actions/authActions';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.loginUser(userData);
+  }
+
   render() {
     return (
       <div id='login-container'>
         <h2>Login Component</h2>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='email'
+            placeholder='Email'
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+          <input
+            type='text'
+            name='password'
+            placeholder='Password'
+            value={this.state.password}
+            onChange={this.onChange}
+          />
+          <button>Login</button>
+        </form>
       </div>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
