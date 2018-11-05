dfbfad07faef63d60c4a1bc82c841908c72708fd
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-04 19:57:21 -0600

    Created initial reducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,20 @@
+import { SET_CURRENT_USER } from '../actions/types';
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch(action) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: action.payload,
+        user: action.payload
+      };
+
+    default:
+      return state;
+  }
+}
