7a71004539272159f59bafdba573342a04917459
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-04 20:00:09 -0600

    Added redux Provider with store

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -8,13 +10,15 @@ import './main.css';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div id='app'>
-          <h1>MERN Social</h1>
-          <Route exact path='/' component={Login} />
-          <Route exact path='/register' component={Register} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div id='app'>
+            <h1>MERN Social</h1>
+            <Route exact path='/' component={Login} />
+            <Route exact path='/register' component={Register} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
