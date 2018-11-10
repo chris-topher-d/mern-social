f842c5b6b756eed9c89c1ecdc3f21ed9565792b4
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-09 22:03:05 -0600

    Added register route for new users

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const keys = require('../config/keys');
+// const passport = require('passport');
 
-// @route  GET api/users/test
+// User model
+const UserModel = require('../models/User');
+
+// @route  GET users/test
 // @desc   Tests users route
 // @access Public
 router.get('/', (req, res) => {
   res.json({msg: 'Users route is working'});
+});
+
+// @route  POST users/test
+// @desc   Register new user
+// @access Public
+router.post('/register', (req, res) => {
+  UserModel.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({email: 'Email already registered'});
+      } else {
+        const newUser = new UserModel({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+
+        newUser.save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
+      }
+    });
 });
 
 // @route GET api/users
