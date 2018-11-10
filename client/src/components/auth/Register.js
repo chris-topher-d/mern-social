8afaad8621bfa35832d84a62425c9bf6aba9b2dc
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-09 22:07:14 -0600

    Created initial registration form and submit action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,81 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { registerUser } from '../../actions/authActions';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      pwConfirm: ''
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    let newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  }
+
   render() {
     return (
       <div id='register'>
-        <h2>Register Compoent</h2>
+        <h2>Register Component</h2>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='name'
+            placeholder='Your name'
+            value={this.state.name}
+            onChange={this.onChange}
+          />
+          <input
+            type='email'
+            name='email'
+            placeholder='Your email'
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='Enter a password'
+            value={this.state.password}
+            onChange={this.onChange}
+          />
+          <input
+            type='password'
+            name='pwConfirm'
+            placeholder='Confirm your password'
+            value={this.state.pwConfirm}
+            onChange={this.onChange}
+          />
+          <button>Create Profile</button>
+        </form>
       </div>
     );
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { registerUser })(Register);
