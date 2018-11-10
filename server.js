b10e5b9c21e1dff69694968561364d2dccd1e76a
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-09 22:06:02 -0600

    Deleted test route

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-const app = express();
-
 // Import routes
 const users = require('./routes/users');
+
+const app = express();
 
 // body-parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,11 +25,6 @@ app.use(passport.initialize());
 
 // Passport config
 require('./config/passport')(passport);
-
-// test route
-app.get('/', (req, res) => {
-  res.send('Test');
-});
 
 // Use routes
 app.use('/users', users);
