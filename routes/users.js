973d6eb7d9e95aa4f7728f5f72fa9b9aad2e239c
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-10 14:59:03 -0600

    Added password encryption for register route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const keys = require('../config/keys');
-// const passport = require('passport');
+const passport = require('passport');
 
 // User model
 const UserModel = require('../models/User');
@@ -13,17 +15,35 @@ router.get('/', (req, res) => {
   res.json({msg: 'Users route is working'});
 });
 
-// @route  POST users/test
-// @desc   Register new user
+// @route  POST users/login
+// @desc   Login user
 // @access Public
 router.post('/login', (req, res) => {
+  // console.log(req.body.email, req.body.password);
+
   UserModel.findOne({ email: req.body.email })
     .then(user => {
-      if (!user) {
-        return res.status(404).json({email: 'User not found'});
-      } else {
-        console.log(user);
-      }
+      // console.log(user);
+      if (!user) return res.status(404).json({email: 'User not found'});
+
+      // Compare provided password with encrypted password
+      bcrypt.compare(req.body.password, user.password)
+        .then(matched => {
+          if (matched) {
+            // User matched
+            // Create JWT payload
+            const payload = { id: user._id, name: user.name };
+            // Sign token
+            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+              res.json({
+                success: true,
+                token: `Bearer ${token}`
+              });
+            });
+          } else {
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        });
     });
 });
 
@@ -42,9 +62,16 @@ router.post('/register', (req, res) => {
           password: req.body.password
         });
 
-        newUser.save()
-          .then(user => res.json(user))
-          .catch(err => console.log(err));
+        // Encrypt password
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) throw err;
+          // Assign hashed password to user object
+          newUser.password = hash;
+          // Save new user to DB
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
       }
     });
 });
