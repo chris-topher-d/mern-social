d68a26a05ec5c67ce2c6415e40b79815f5cf297a
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-15 23:18:23 -0600

    Creating initial user profile layout

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Dashboard extends Component {
+
   render() {
+    let { user } = this.props.auth;
+
     return (
       <div id='dashboard'>
         <h2>Dashboard</h2>
+        <div id='user-profile'>
+          {user.name}
+        </div>
       </div>
     );
   }
