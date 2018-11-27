02fc71c3c73ab969ea2526b50d7a5482d2b95a6d
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-27 00:16:01 -0600

    Added PROFILE_LOADING action type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const PROFILE_LOADING = 'PROFILE_LOADING';
 export const GET_PROFILE = 'GET_PROFILE';
 export const GET_ERRORS = 'GET_ERRORS';
