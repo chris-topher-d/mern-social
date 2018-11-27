b408518e9d0f69ece861c88ab9f9bab2f5b3cdeb
Christopher Dennis <cdennis.aus@gmail.com>
2018-11-26 23:58:24 -0600

    Created dashboard landing for users with no profile yet

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,12 +7,22 @@ class Dashboard extends Component {
   render() {
     let { user } = this.props.auth;
 
+    // If user doesn't have a profile yet
+    let dashboard = (
+      <div class='dashboard'>
+        <header>
+          <h2>Welcome {user.name}</h2>
+        </header>
+        <p>Your profile has no information. Please provide some details about yourself.</p>
+        <Link to='/create-profile' className='link-btn'>
+          Create Profile
+        </Link>
+      </div>
+    );
+
     return (
-      <div id='dashboard'>
-        <h2>Dashboard</h2>
-        <div id='user-profile'>
-          {user.name}
-        </div>
+      <div id='dashboard-container'>
+        {dashboard}
       </div>
     );
   }
